984384264634604f36dd4f509b670fb4e64e66db
Guillem <guillem@disedit.com>
2025-12-16 20:24:12 +0100

    add page nav

--- app/composables/useSpacing.js
+++ app/composables/useSpacing.js
@@ -1,15 +1,17 @@
-export const useSpacing = (blok, defaultPadding, defaultMargin) => {
-  if (!defaultPadding) defaultPadding = '6rem'
+export const useSpacing = (blok, defaultPadding, defaultMargin, defaultPaddingDesktop, defaultMarginDesktop) => {
+  if (!defaultPadding) defaultPadding = 'calc(var(--spacing-site) * 2)'
+  if (!defaultPaddingDesktop) defaultPaddingDesktop = '6rem'
   if (!defaultMargin) defaultMargin = 0
+  if (!defaultMarginDesktop) defaultMarginDesktop = 0
 
   return ref({
     '--pt': blok.padding_top  || defaultPadding,
     '--pb': blok.padding_bottom  || defaultPadding,
-    '--pt-md': blok.padding_top_desktop  || defaultPadding,
-    '--pb-md': blok.padding_bottom_desktop || defaultPadding,
+    '--pt-md': blok.padding_top_desktop  || defaultPaddingDesktop,
+    '--pb-md': blok.padding_bottom_desktop || defaultPaddingDesktop,
     '--mt': blok.margin_top || defaultMargin,
     '--mb': blok.margin_bottom || defaultMargin,
-    '--mt-md': blok.margin_top_desktop || defaultMargin,
-    '--mb-md': blok.margin_bottom_desktop || defaultMargin
+    '--mt-md': blok.margin_top_desktop || defaultMarginDesktop,
+    '--mb-md': blok.margin_bottom_desktop || defaultMarginDesktop
   })
 }
---
+++ app/storyblok/PageNav.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <nav class="p-site md:p-24 pb-0 md:pb-0 overflow-x-auto">
+    <ul aria-label="Navegación interna de la página" class="flex text-base md:text-md gap-site justify-center">
+      <li v-for="item in blok.items" :key="item._uid">
+        <a :href="`#${item.anchor}`" class="to-underlined">
+          {{ item.label }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
